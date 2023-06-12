a5b88004f1bc83647e7999879bbfc5a3f56a6a56
Brett Green <brettalangreen@gmail.com>
2023-06-12 16:48:51 -0500

    remove console.logs, formatting

--- src/JokeList.js
+++ src/JokeList.js
@@ -17,19 +17,16 @@ class JokeList extends React.Component {
 	}
 
 	vote(id, delta) {
-		console.log('id', id);
-		console.log('delta', delta);
-		console.log('jokes', this.state.jokes);
-		
 		const newJokes = this.state.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j));
 		this.setState({jokes: newJokes});
+		
 	}
 
 	async getJokes() {
-		console.log('getJokes',this.state.jokes)
 
 		let j = [...this.state.jokes];
 		let seenJokes = new Set();
+
 		try {
 			while (j.length < this.numJokesToGet) {
 				let res = await axios.get("https://icanhazdadjoke.com", {
@@ -44,29 +41,28 @@ class JokeList extends React.Component {
 					console.error("duplicate found!");
 				}
 			}
+
 			this.setState({jokes: j});
+
 		} catch (e) {
 			console.log(e);
 		}
 	}
 
 	componentDidMount() {
-		console.log('mountJokes',this.state.jokes)
 		if (this.state.jokes.length === 0) {
 			this.getJokes();
 		}
 	}
 
 	componentDidUpdate() {
-		console.log('updateJokes',this.state.jokes)
-
 		if (this.state.jokes.length === 0) {
 			this.getJokes();
 		}
 	}
 
 	render() {
-		console.log('renderJokes',this.state.jokes)
+
 		if (this.state.jokes.length) {
 			let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
 		  
